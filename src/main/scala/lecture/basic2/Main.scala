948636c6851302df98db23f572829b144763a384
takehidesoh <takehide.soh@gmail.com>
2020-05-21 10:49:59 +0900

    update

--- src/main/scala/lecture/basic2/Main.scala
+++ src/main/scala/lecture/basic2/Main.scala
@@ -8,8 +8,8 @@ object Main extends medianSpec {
 
     def main(args: Array[String]) = {
         val example1 = Seq(1,2,3)
-        val example2 = Seq(100,80,200,40,500,50)
-        val example3 = Seq(9,999,10,20)        
+        val example2 = Seq(100,99,200,40,500,50)
+        val example3 = Seq(9,999,19,20)        
         val example4 = Seq(333,3,33)                
         println(s"$example1 の中央値は ${median(example1)}")
         println(s"$example2 の中央値は ${median(example2)}")
